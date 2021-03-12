287a7a3b93d18a6ee4a8ec48b179d19e95397ca7
Anton788 <anton.ilin080599@yandex.ru>
2021-03-12 23:00:00 +0300

    hw1

--- triangl/triangle.cpp
+++ triangl/triangle.cpp
@@ -1,4 +1,4 @@
-#include <stdio.h>
+п»ї#include <stdio.h>
 #include <stdlib.h>
 
 #include <GL/glew.h>
@@ -52,130 +52,51 @@ int main(void)
 	// Ensure we can capture the escape key being pressed below
 	glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
 
-	// Dark blue background
-	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+	// background
+	glClearColor(0.1f, 0.f, 0.1f, 0.0f);
 
-
-	glEnable(GL_DEPTH_TEST);
-	glDepthFunc(GL_LESS);
 
 	GLuint VertexArrayID;
 	glGenVertexArrays(1, &VertexArrayID);
 	glBindVertexArray(VertexArrayID);
 
-	GLuint programID = LoadShaders("SimpleTransform.vertexshader", "SingleColor.fragmentshader");
+	GLuint programID = LoadShaders("SimpleVertexShader.vertexshader", "SimpleFragmentShader.fragmentshader");
 
 	GLuint MatrixID = glGetUniformLocation(programID, "MVP");
 
+	const float r = 7.0f;
 	glm::mat4 Projection = glm::perspective(glm::radians(30.0f), 4.0f / 3.0f, 0.1f, 100.0f);
 
 
 	static const GLfloat g_vertex_buffer_data[] = {
-		//	Основание
-		0.0f, 0.0f, 0.0f,
-		-0.5f, -0.02f, 0.0f,
-		-0.25f, -0.45f, 0.0f,
-		
-		0.0f, 0.0f, 0.0f,
-		-0.25f, -0.45f, 0.0f,
-		0.25f, -0.45f, 0.0f,
+		  -1.0f, -1.0f, 1.0f,
+		  -0.5f, 1.0f, 1.0f,
+		  0.5f, 0.0f, 0.0f,
 
-		0.0f, 0.0f, 0.0f,
-		0.25f, -0.45f, 0.0f,
-		0.5f, -0.01f, 0.0f,
-
-		0.0f, 0.0f, 0.0f,
-		0.5f, -0.01f, 0.0f,
-		0.24f, 0.42f, 0.0f,
-
-		0.0f, 0.0f, 0.0f,
-		0.24f, 0.42f, 0.0f,
-		-0.26f, 0.42f, 0.0f,
-
-		0.0f, 0.0f, 0.0f,
-		-0.26f, 0.42f, 0.0f,
-		-0.5f, -0.02f, 0.0f,
-
-		//Веришина
-		0.0f, 0.0f, 1.0f,
-		-0.5f, -0.02f, 0.0f,
-		-0.25f, -0.45f, 0.0f,
-
-		0.0f, 0.0f, 1.0f,
-		-0.25f, -0.45f, 0.0f,
-		0.25f, -0.45f, 0.0f,
-
-		0.0f, 0.0f, 1.0f,
-		0.25f, -0.45f, 0.0f,
-		0.5f, -0.01f, 0.0f,
-
-		0.0f, 0.0f, 1.0f,
-		0.5f, -0.01f, 0.0f,
-		0.24f, 0.42f, 0.0f,
-
-		0.0f, 0.0f, 1.0f,
-		0.24f, 0.42f, 0.0f,
-		-0.26f, 0.42f, 0.0f,
-
-		0.0f, 0.0f, 1.0f,
-		-0.26f, 0.42f, 0.0f,
-		-0.5f, -0.02f, 0.0f,
+		  1.0f, -1.0f, 1.0f,
+		  1.0f, 0.5f, 1.0f,
+		  -1.0f, 0.0f, 0.0f,
 	};
+
 	GLuint vertexbuffer;
+
 	glGenBuffers(1, &vertexbuffer);
+
 	glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+
 	glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
 
 	GLfloat g_color_buffer_data[] = {
 		255, 0, 51,
 		255, 0, 51,
-		255, 0, 51,
-
-		255, 0, 51,
-		255, 0, 51,
-		255, 0, 51,
+		255, 255, 0,
 		
-		255, 0, 51,
-		255, 0, 51,
-		255, 0, 51,
+		75, 51, 255,
+		75, 51, 255,
+		51, 255, 238,
 
-		255, 0, 51,
-		255, 0, 51,
-		255, 0, 51,
-
-		255, 0, 51,
-		255, 0, 51,
-		255, 0, 51,
-
-		255, 0, 51,
-		255, 0, 51,
-		255, 0, 51,
-
-		255, 255, 0,
-		255, 0, 51,
-		255, 0, 51,
-
-		255, 255, 0,
-		255, 0, 51,
-		255, 0, 51,
-
-		255, 255, 0,
-		255, 0, 51,
-		255, 0, 51,
-
-		255, 255, 0,
-		255, 0, 51,
-		255, 0, 51,
-
-		255, 255, 0,
-		255, 0, 51,
-		255, 0, 51,
-
-		255, 255, 0,
-		255, 0, 51,
-		255, 0, 51,
 	};
-	for (int i = 0; i < 36 * 3; i++) {
+	for (int i = 0; i < 18; i++) {
 		g_color_buffer_data[i] = g_color_buffer_data[i] / 255;
 	}
 	GLuint colorbuffer;
@@ -183,18 +104,18 @@ int main(void)
 	glBindBuffer(GL_ARRAY_BUFFER, colorbuffer);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(g_color_buffer_data), g_color_buffer_data, GL_STATIC_DRAW);
 
-	const float r = 5.0f;
+	// Enable blending
 	glEnable(GL_BLEND);
 	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
 	do {
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 		glUseProgram(programID);
 
+		float x_camer = r * sin(0.5 * glfwGetTime());
+		float y_camer = r * sin(0.1 * glfwGetTime());
+		float z_camer = r * cos(0.5 * glfwGetTime());
 		
-		float x_camer = r * sin(glfwGetTime());
-		float y_camer = r * sin(glfwGetTime());
-		float z_camer = r * cos(glfwGetTime());
-
 		glm::mat4 View = glm::lookAt(
 			glm::vec3(x_camer, y_camer, z_camer),
 			glm::vec3(0, 0, 0),
@@ -203,40 +124,37 @@ int main(void)
 		
 		glm::mat4 Model = glm::mat4(1.0f);
 
-		
 		glm::mat4 MVP = Projection * View * Model;
 
 
 		
 		glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &MVP[0][0]);
 
-		
 		glEnableVertexAttribArray(0);
 		glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
 		glVertexAttribPointer(
-			0,      
-			3,      
-			GL_FLOAT,
-			GL_FALSE,
-			0,       
-			(void*)0 
+			0,                  
+			3,                
+			GL_FLOAT,          
+			GL_FALSE,           
+			0,                  
+			(void*)0            
 		);
 		
 		glEnableVertexAttribArray(1);
 		glBindBuffer(GL_ARRAY_BUFFER, colorbuffer);
 		glVertexAttribPointer(
-			1,  
-			3,      
-			GL_FLOAT,
-			GL_FALSE,
-			0,       
-			(void*)0 
+			1,                                
+			3,                                
+			GL_FLOAT,                         
+			GL_FALSE,                         
+			0,                              
+			(void*)0                         
 		);
 
-		glDrawArrays(GL_TRIANGLES, 0, 36);
+		glDrawArrays(GL_TRIANGLES, 0, 6);
 
 		glDisableVertexAttribArray(0);
-		// Draw nothing, see you in tutorial 2 !
 
 		// Swap buffers
 		glfwSwapBuffers(window);
